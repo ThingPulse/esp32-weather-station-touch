374afd77c9dbb5d35fe2904e6265c5e12b8b66a0
Daniel Eichhorn <dani.eichhorn@squix.ch>
2025-01-12 13:00:39 +0100

    Add dutch translations

---
+++ src/translations/texts_nl.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// Supported languages: https://openweathermap.org/current#multi
+const String OPEN_WEATHER_MAP_LANGUAGE = "nl";
+
+const String WEEKDAYS[] = {"Zondag", "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag"};
+const String WEEKDAYS_ABBR[] = {"ZO", "MA", "DI", "WOE", "DO", "FRIJ", "ZA"};
+
+const String SUN_MOON_LABEL[] = {"Zon", "Maan"};
+const String MOON_PHASES[] = {"Nieuwe Maan", "Jonge Maansikkel", "Eerste Kwartier", "Wassende Maan",
+                              "Volle Maan", "Krimpende Maan", "Laatste Kwartier", "Krimpende Maan"};
+							  
+//	Nieuwe maan - vaak afgekort: NM.
+//  Wassende, sikkelvormige maan of jonge maansikkel.
+//  Eerste kwartier - afgekort: EK.
+//  Wassende maan.
+//  Volle maan - afgekort: VM.
+//  Krimpende of afnemende maan.
+//  Laatste kwartier.
+//  Krimpende, sikkelvormige maan of asgrauwe maan.
