8ea90d46b5f377f609b42b4ed4262f75c2659f00
Marcel Stör <marcelstoer@users.noreply.github.com>
2025-07-26 12:30:14 +0200

    Fix typo in Dutch WEEKDAYS_ABBR
    Fixes #5

--- src/translations/texts_nl.h
+++ src/translations/texts_nl.h
@@ -4,12 +4,12 @@
 const String OPEN_WEATHER_MAP_LANGUAGE = "nl";
 
 const String WEEKDAYS[] = {"Zondag", "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag"};
-const String WEEKDAYS_ABBR[] = {"ZO", "MA", "DI", "WOE", "DO", "FRIJ", "ZA"};
+const String WEEKDAYS_ABBR[] = {"ZO", "MA", "DI", "WOE", "DO", "VRIJ", "ZA"};
 
 const String SUN_MOON_LABEL[] = {"Zon", "Maan"};
 const String MOON_PHASES[] = {"Nieuwe Maan", "Jonge Maansikkel", "Eerste Kwartier", "Wassende Maan",
                               "Volle Maan", "Krimpende Maan", "Laatste Kwartier", "Krimpende Maan"};
-							  
+
 //	Nieuwe maan - vaak afgekort: NM.
 //  Wassende, sikkelvormige maan of jonge maansikkel.
 //  Eerste kwartier - afgekort: EK.
